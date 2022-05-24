cb0085e6f3841222217a81d00d2f54d5c6a4c719
Mau <mauriciom.curiel@gmail.com>
2022-05-24 15:24:02 -0700

    added ball movement

--- sketch.js
+++ sketch.js
@@ -3,12 +3,13 @@ const playerWidth = 200,
     spinSpeed = 1,
     ballDiam = 40;
 
-let radius, screenCenter, masterVel, xVel, yVel;
+let radius, screenCenter, xVel, yVel;
 let p1angle = 90,
     p2angle = 90,
     multiplayer = true,
     ballX = 0,
-    ballY = 0;
+    ballY = 0,
+    masterVel = 5;
 
 
 function setup() {
@@ -16,6 +17,10 @@ function setup() {
     radius = Math.min(width, height)*0.45
     screenCenter = createVector(width/2, height/2)
     angleMode(DEGREES)
+    
+    xVel = Math.random()*masterVel*2-(masterVel) //[-masterVel,masterVel]
+    var yDirection = Math.random() < 0.5 ? -1 : 1 //randomly is negative or positive
+    yVel = Math.sqrt(Math.pow(masterVel,2) - Math.pow(xVel,2)) * yDirection//completes a^2+b^2=c^2
 }
   
 function draw() {
@@ -31,6 +36,8 @@ function draw() {
 }
 
 function drawBall(){
+    ballX+=xVel
+    ballY+=yVel
     circle(ballX, ballY, ballDiam)
 }
 
