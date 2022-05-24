542ccc26ebebf6684e2e5871e377be22a5ed962a
Mau <mauriciom.curiel@gmail.com>
2022-05-24 15:10:38 -0700

    added spacebackground and ball

--- sketch.js
+++ sketch.js
@@ -1,24 +1,65 @@
-
-let radius, center, testAngle;
 const playerWidth = 200,
-    playerHeight = 50;
+    playerHeight = 50,
+    spinSpeed = 1,
+    ballDiam = 40;
+
+let radius, screenCenter, masterVel, xVel, yVel;
+let p1angle = 90,
+    p2angle = 90,
+    multiplayer = true,
+    ballX = 0,
+    ballY = 0;
+
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight-1);
     radius = Math.min(width, height)*0.45
-    center = createVector(width/2, height/2)
-    testAngle = 0
+    screenCenter = createVector(width/2, height/2)
+    angleMode(DEGREES)
 }
   
 function draw() {
-    background(220);
-    noFill()
-    circle(center.x, center.y, radius*2)
-    testAngle+= 0.01
+    clear()
+    translate(screenCenter)
     push()
-    translate(center)
-    rotate(testAngle)
+    noFill()
+    circle(0, 0, radius*2)
+    pop()
+    controls()
+    drawPlayers()
+    drawBall()
+}
+
+function drawBall(){
+    circle(ballX, ballY, ballDiam)
+}
+
+function bounce(){
+
+}
+
+function drawPlayers(){
+    push()
+    rotate(p1angle)
     rect(-playerWidth/2, -radius - playerHeight/2, playerWidth, playerHeight)
+    pop()
+    push()
+    rotate(p2angle)
     rect(-playerWidth/2, radius - playerHeight/2, playerWidth, playerHeight)
     pop()
+}
+
+function controls(){
+    if (keyIsDown(RIGHT_ARROW)){
+        p1angle += spinSpeed
+    }
+    if (keyIsDown(LEFT_ARROW)){
+        p1angle -= spinSpeed
+    }
+    if (keyIsDown(65) && multiplayer){
+        p2angle -= spinSpeed
+    }
+    if (keyIsDown(68) && multiplayer){
+        p2angle += spinSpeed
+    }
 }
